87f5be05bcc3aa9afa0fc61d8419197dd86f6a6c
Esbeidy Abigail Chi Xool <echi@telstock.net>
2025-06-08 01:13:08 -0600

    Actualizacion

--- Semana1/2-Programa-InyeccionDeDependencia/src/NotificacionEmail.java
+++
@@ -1,8 +0,0 @@
-//Clase de Notificaciones por Email en donde se implemente la interfaz del servicio del mensaje 
-public class NotificacionEmail implements ServicioMensaje{
-	//Se implementa el metodo de la interfaz y se utiliza el Override para sobreescribir el metodo, esto solo imprime un mensaje indicando que es por EMAIL
-	@Override
-	public void enviaMensaje(String mensaje) {
-		System.out.println("Se esta enviando la notificacion por EMAIL: " + mensaje);
-	}
-}
--- Semana1/2-Programa-InyeccionDeDependencia/src/ServicioMensaje.java
+++
@@ -1,6 +0,0 @@
-
-//Esta es una interface que solo tiene el metodo de enviaMensaje que recibe como parametro el mensaje
-public interface ServicioMensaje {
-
-	void enviaMensaje(String mensaje);
-}
